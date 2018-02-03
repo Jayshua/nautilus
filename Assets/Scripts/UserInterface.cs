2975286ed35cccb2744e168bdcb7c184053dce38
Jayshua Nelson <me@jayshuanelson.com>
2018-02-03 16:16:41 -0600

    Added some images for the user interface

---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour {
+	public void ShowUserSelection() {}
+	public void ShowClassSelection() {
+	}
+}
