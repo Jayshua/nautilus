b1d4bd553e9cfdc1d70f8e24def27a7f56297d7d
Conner Enders <126675@studentnet.int>
2018-02-09 17:30:39 -0600

    Modified camera follow script

--- Assets/Scripts/Ship/CameraFollow.cs
+++ Assets/Scripts/Ship/CameraFollow.cs
@@ -5,21 +5,50 @@ using UnityEngine.Networking;
 
 public class CameraFollow : MonoBehaviour {
 
+	// The target we are following
 	Transform player;
+	// The distance in the x-z plane to the target
+	public float distance = 40.0f;
+	// the height we want the camera to be above the target
+	public float height = 80.0f;
+	// How much we 
+	public float heightDamping = 2.0f;
+	public float rotationDamping = 3.0f;
 
-	// Use this for initialization
-	void Awake () {
-	}
-	
-	// Update is called once per frame
+	// Place the script in the Camera-Control group in the component menu
+	[AddComponentMenu("Camera-Control/Smooth Follow")]
+
 	void LateUpdate () {
-		if (player != null) {
-			transform.position = player.position;
-			transform.Translate (0,100,-100, Space.World);
-			transform.LookAt (player);
-		}
-	}
+		// Early out if we don't have a target
+		if (!player) return;
 
+		// Calculate the current rotation angles
+		float wantedRotationAngle = player.eulerAngles.y;
+		float wantedHeight = player.position.y + height;
+
+		float currentRotationAngle = transform.eulerAngles.y;
+		float currentHeight = transform.position.y;
+
+		// Damp the rotation around the y-axis
+		currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+		// Damp the height
+		currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		// Convert the angle into a rotation
+		var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+		// Set the position of the camera on the x-z plane to:
+		// distance meters behind the target
+		transform.position = player.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+
+		// Set the height of the camera
+		transform.position = new Vector3(transform.position.x,currentHeight,transform.position.z);
+
+		// Always look at the target
+		transform.LookAt(player);
+	}
 	public void PlayerCreated(Transform player) {
 		this.player = player;
 	}
