d4c88e6af406f423eece8431a8d2b42ecd599173
JD Isherwood <jisher0055@students.pcci.edu>
2018-02-16 12:26:01 -0600

    Added temp PowerUp Icons to main and added them to GUI script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,6 +28,7 @@ public class Player : NetworkBehaviour
 
 			if (OnGoldChange != null) {
 				OnGoldChange (this);
+				OnAddPowerups (this);
 			}
 		}
 	}
@@ -52,6 +53,7 @@ public class Player : NetworkBehaviour
 	public event Action<Player> OnLogout;
 	public event Action<Player> OnGoldChange;
 	public event Action<Player> OnFameChange;
+	public event Action<Player> OnAddPowerups;
 
 	[Server]
 	public void Setup(string playerName, NetworkConnection playerConnection) {
